520a074e880f142b7ed62a24b7eb12349ea3d454
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-02-18 19:50:38 -0500

    add-btn & close-btn styling

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,40 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import SongList from "./components/SongList";
 
+import { BsPlusCircle } from 'react-icons/bs';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+
 function App() {
   const [songList, setSongList] = useState([
     {},
     {},
     {}
   ]);
+
+  const handleAdd = () => {
+    setSongList(oldArray => [...oldArray, {}]);
+  }
+
   return (
-    <div>
+    <div className="app">
       <Navbar />
-      {
-        songList.map((val, idx) => {
-          return (
-            <div>
-              <SongList />
-            </div>
-          );
-        })
-      }
+      <div className="song-list-container">
+        {
+          songList.map((val, idx) => {
+            return (
+              <div className="song-list-wrapper">
+                <div className="close-btn" onClick={handleAdd}>
+                  <AiOutlineCloseCircle className="close-btn-icon" />
+                </div>
+                <SongList />
+              </div>
+            );
+          })
+        }
+        <div className="add-btn" onClick={handleAdd}>
+          <BsPlusCircle />
+        </div>
+      </div>
       <Footer />
     </div>
   );
