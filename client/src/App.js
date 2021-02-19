aa0208d336c20ae002132c2d02a818e397ef2796
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-02-19 17:19:25 -0500

    Move SongList data into top-level App.js
    before App.js was empty

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,97 @@ import { BsPlusCircle } from 'react-icons/bs';
 
 function App() {
   const [listSongLists, setListSongLists] = useState([
-    {},
-    {},
-    {}
+    {
+      title: 'Favorite Songs',
+      songList: [
+        {
+          text: "Learn about React",
+          isCompleted: false
+        },
+        {
+          text: "Meet friend for lunch",
+          isCompleted: false
+        },
+        {
+          text: "Build really cool todo app; Build really cool todo app; Build really cool todo app; Build really cool todo app; Build really cool todo app",
+          isCompleted: false
+        }
+      ]
+    },
+    {
+      title: 'Favorite Songs',
+      songList: [
+        {
+          text: "Learn about React",
+          isCompleted: false
+        },
+        {
+          text: "Meet friend for lunch",
+          isCompleted: false
+        },
+        {
+          text: "Build really cool todo app; Build really cool todo app; Build really cool todo app; Build really cool todo app; Build really cool todo app",
+          isCompleted: false
+        }
+      ]
+    },
+    {
+      title: 'Favorite Songs',
+      songList: [
+        {
+          text: "Learn about React",
+          isCompleted: false
+        },
+        {
+          text: "Meet friend for lunch",
+          isCompleted: false
+        },
+        {
+          text: "Build really cool todo app; Build really cool todo app; Build really cool todo app; Build really cool todo app; Build really cool todo app",
+          isCompleted: false
+        }
+      ]
+    },
   ]);
   
-  const removeSongList = index => {
+  // apply functions on individual song lists
+  const addSongListItem = (songListIdx, text) => {
+    const newArr = [...listSongLists]; // copy array
+    const oldSongs = newArr[songListIdx].songList; 
+    const newSongs = [...oldSongs, { text }]; // operation on SongListItem
+    newArr[songListIdx] = {
+      title: newArr[songListIdx].title,
+      songList: newSongs
+    };
+    setListSongLists(newArr);
+  };
+
+  const completeSongListItem = (songListIdx, songItemIdx) => {
+    const newArr = [...listSongLists]; // copy array
+    const oldSongs = newArr[songListIdx].songList;
+    const newSongs = [...oldSongs];
+    newSongs[songItemIdx].isCompleted = true; // operation on SongListItem
+    newArr[songListIdx] = {
+      title: newArr[songListIdx].title,
+      songList: newSongs
+    };
+    setListSongLists(newArr);
+  };
+
+  const removeSongListItem = (songListIdx, songItemIdx) => {
+    const newArr = [...listSongLists]; // copy array
+    const oldSongs = newArr[songListIdx].songList; 
+    const newSongs = [...oldSongs];
+    newSongs.splice(songItemIdx, 1); // operation on SongListItem
+    newArr[songListIdx] = {
+      title: newArr[songListIdx].title,
+      songList: newSongs
+    };
+    setListSongLists(newArr);
+  };
+  
+  // apply functions on list of song lists
+  const removeListSongList = index => {
     const newTodos = [...listSongLists];
     newTodos.splice(index, 1);
     setListSongLists(newTodos);
@@ -32,12 +117,17 @@ function App() {
         <div className="song-list-container">
           {
             listSongLists.map((val, idx) => {
+              const { songList } = val;
               return (
                 <SongList
                   key={idx}
-                  index={idx}
+                  songListIdx={idx}
                   title={"Favorite Songs"}
-                  removeSongList={removeSongList}
+                  removeSongList={removeListSongList}
+                  completeTodo={completeSongListItem}
+                  removeTodo={removeSongListItem}
+                  addTodo={addSongListItem}
+                  songs={songList}
                 />
               );
             })
--- client/src/components/SongList/SongListWrapper.js
+++ client/src/components/SongList/SongListWrapper.js
@@ -1,12 +1,48 @@
 import React, { useState } from 'react';
 // custom components
-import SongList from './SongList';
 import InlineEdit from './InlineEdit';
 import DropdownMenu from './DropdownMenu';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 
+function Todo({ songListIdx, todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      className="song"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+      <div style={{margin: '5px'}}>
+        <button onClick={() => completeTodo(songListIdx, index)}>Complete</button>
+        <button onClick={() => removeTodo(songListIdx, index)}>x</button>
+      </div>
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
 export default function SongListWrapper(props) {
-  const { title, index, removeSongList } = props;
+  const { completeTodo, removeTodo, addTodo, songs, title, songListIdx, removeSongList } = props;
   const [storedHeading, setStoredHeading] = useState(
     title
   );
@@ -17,8 +53,14 @@ export default function SongListWrapper(props) {
       text: "Delete",
       execFunc: () => {
         return (
-          removeSongList(index)
+          removeSongList(songListIdx)
         )
+      }
+    },
+    {
+      text: "BLAH",
+      execFunc: () => {
+        console.log("BLAH");
       }
     }
   ]
@@ -35,7 +77,19 @@ export default function SongListWrapper(props) {
           onSetText={text => setStoredHeading(text)}
         />
       </h3>
-      <SongList />
+      <div className="song-list">
+        {songs.map((todo, index) => (
+          <Todo
+            key={index}
+            songListIdx={songListIdx}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   )
 }
