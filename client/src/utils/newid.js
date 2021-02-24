fb9af95c1266a1c5cc8cab2d6859502f9f08bf66
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-02-24 04:34:28 -0500

    fix to store lastId in localStorage so list can continue to increment

--- client/src/utils/newid.js
+++ client/src/utils/newid.js
@@ -1,6 +1,16 @@
-let lastId = 0;
 
-export default function(prefix='id') {
-    lastId++;
-    return `${prefix}${lastId}`;
+let lastId;
+
+// if null, use default value
+if (localStorage.getItem('lastId') === null) {
+  lastId = 0;
+} else {
+  lastId = localStorage.getItem('lastId');
+}
+
+export default function (prefix = 'id') {
+  console.log(lastId)
+  lastId++;
+  localStorage.setItem('lastId', JSON.stringify(lastId));
+  return `${prefix}${lastId}`;
 }
